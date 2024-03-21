8cc9b79b25d39f9e3b37439abd6b64be343ac7c3
Soraya-j <21255@ecam.be>
2024-03-21 20:02:15 +0100

    2eme page

--- server.js
+++ server.js
@@ -12,28 +12,10 @@ app.get('/', (request, response) => {
     response.render('home.ejs', { tasks, successMessage: null, errorMessage: null });
 });
 
-//Lorsqu'on essaye d'ajouter une tache
-app.post('/addTask', (request, response) => {
-    const task = request.body.task;
-    if (task.trim() !== '') {
-        tasks.push(task);
-        response.render('home.ejs', { tasks, successMessage: 'Ajout réussi', errorMessage: null });
-    } 
-    else {
-        response.render('home.ejs', { tasks, successMessage: null, errorMessage: 'Complétez la tâche' });
-    }
+// Route pour la page 'about'
+app.get('/amisSession', (request, response) => {
+    response.render('amisSession.ejs'); // Assurez-vous d'avoir un fichier 'about.ejs' dans votre dossier de vues
 });
-
-// Supprimer une tâche
-app.get('/delete', (request, response) => {
-    const task = request.query.task;
-    tasks = tasks.filter(t => t !== task); //tout ce qui est diff de la tache a sup, il garde 
-    response.redirect('/');
-  });
-  
-app.get('/', (request, response) => {
-    response.redirect('/');
-  });
 
 app.listen(80, () => {
     console.log('Server is running on port 80');
